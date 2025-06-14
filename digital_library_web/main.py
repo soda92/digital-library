701b691ef456290387db950882cb9c997bbd8d0f
Maple <wjxa20152015@gmail.com>
2025-06-14 12:18:05 +0800

    update deno command, add auto deploy

--- digital_library_web/main.py
+++ digital_library_web/main.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 import subprocess
 from sodatools import CD
+import platform
 
+deno_command = (
+    "deno" if platform.system() == "Windows" else Path.home().joinpath(".deno/bin/deno")
+)
 CR = Path(__file__).resolve().parent
 
 
 def main():
     with CD(str(CR)):
-        subprocess.run("deno task build", shell=True)
-        subprocess.run("deno run --allow-net --allow-env --allow-read main.ts", shell=True)
+        subprocess.run(f"{deno_command} task build", shell=True)
+        subprocess.run(
+            f"{deno_command} --allow-net --allow-env --allow-read main.ts", shell=True
+        )
 
 
 if __name__ == "__main__":
